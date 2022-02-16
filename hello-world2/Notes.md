a6f67a2f43249ff6c3cd3a56cb02f233f0e40206
katherin <ft.katheochoa@gmail.com>
2022-02-15 22:21:17 -0500

    added status error and added some dependencies

---
+++ hello-world2/Notes.md
@@ -0,0 +1,15 @@
+#
+
+1. **mkdir nameoftheproject**
+2. **cd nameoftheprojects**
+3. sam init
+
+```
+     --no-interactive \
+     --runtime nodejs12.x \
+     --dependency-manager npm \
+     --output-dir . \
+     --name simple-node-api \
+     --app-template hello-world
+
+```
--- hello-world2/hello-world/app.js
+++ hello-world2/hello-world/app.js
@@ -4,9 +4,20 @@
 
 const serverless = require("serverless-http");
 const express = require("express");
+const s3 = require("aws-sdk/clients/s3");
+const v5 = require("uuid/v5");
+const env = require("env-var");
+const bodyParser = require("body-parser");
 
 const app = new express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.get("/", (err, _, res, _) => {
+  console.log(err);
+  res.status(500).json({ message: "Internal Server Error" });
 });
+
+function newS3Client() {
+  return new s3({ params: { Bucket: env.get("BUCKET").required() } });
+}
+
+module.exports.lambdaHandler = serverless(app);
